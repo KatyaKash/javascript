133b6b0e6e851608e100ddc0b5bbf3c0886c4081
KatyaKash <katya_kash@inbox.ru>
2022-02-20 12:50:49 +0700

    02.07 задание первое

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-let S = 2000000;
-let p =0.1;
-let years = 5;
-let perepl;
+hide = true;
+function sayHello(){
+	if (hide == true){
+		document.getElementById('kartinka').src = 'https://media.kg-portal.ru/anime/t/tokyoghoul/trailers/19643t.jpg'
+		hide = false;
+	}
+	else {
+		document.getElementById('kartinka').src = '';
+		hide = true
+	}
 
-perepl = S*p*years;
-console.log(`переплата по кредиту - ${perepl}`);
+
+}
