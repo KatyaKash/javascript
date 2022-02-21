15c632af28d7996170e52279bc83d12922cbb230
Yuri Kirsanov <katya_kash@inbox.ru>
2022-02-21 10:49:30 +0700

    studentsss

--- main.js
+++ main.js
@@ -6,12 +6,60 @@ let students = [
 {id: 5, name: 'matvei', surname:'gavrilov'}
 ]
 
-for (let i = 0; i < students.length; i++) {
-	let id = students[i].id
-	let name = students[i].name
-	let surname = students[i].surname
-	console.log('number: ' + id)
-	console.log('name: ' + name)
-	console.log('surname: ' + surname)
-	console.log('-------------------')
+function load_all() {
+	let table = document.getElementById('tbl_all')
+	for (let i = 0; i < students.length; i++) {
+		let id = students[i].id
+		let name = students[i].name
+		let surname = students[i].surname
+
+		//создадим стрку и 3 ячейки
+		let tr = document.createElement('tr')
+		let td1 = document.createElement('td')
+		let td2 = document.createElement('td')
+		let td3 = document.createElement('td')
+		//запишем данные в ячейк
+		td1.textContent = id;
+		td2.textContent = name;
+		td3.textContent = surname;
+		//вставим ячейку в строку
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		//вставим строку в таблицу
+		table.appendChild(tr)
+	}
+}
+let id_current = 0
+function load_student(id){
+	let head = document.getElementById('zagolovok')
+	head.textContent = 'Info about student № '+ students[id].id
+
+	let span1 = document.getElementById('name')
+	span1.textContent = students[id_current].name
+	let span2 = document.getElementById('surname')
+	span2.textContent = students[id_current].surname
+}
+
+function Next(){
+	id_current++
+	if (id_current > 0) {
+		document.getElementById('btn_prev').disabled = false
+	}
+	if (id_current===students.length-1){
+		document.getElementById('btn_next').disabled = true
+
+	}
+	load_student(id_current)
+}
+function Prev()	{
+		id_current--
+	if (id_current === 0) {
+		document.getElementById('btn_prev').disabled = true
+	}
+	if (id_current > 0){
+		document.getElementById('btn_next').disabled = false
+
+	}
+	load_student(id_current)
 }
