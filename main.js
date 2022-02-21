f3e7d60558b4862d2a646409581f896a4e2ccefc
Yuri Kirsanov <katya_kash@inbox.ru>
2022-02-21 09:08:18 +0700

    raz

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-let id = 1;
-function sayHello(){
-	let table = document.getElementById('tbl');
-	let name = document.getElementById('name').value;
-	let tr = document.createElement('tr');
-	let td1 = document.createElement('td');
-	let td2 = document.createElement('td');
-	
-	td1.textContent = id;
-	td2.textContent = name;
-	tr.appendChild(td1);
-	tr.appendChild(td2);
-	table.appendChild(tr);
+let students = [
+{id: 1, name: 'andrey', surname:'artamonov'},
+{id: 2, name: 'daria', surname:'arhipova'},
+{id: 3, name: 'nicolai', surname:'barkalov'},
+{id: 4, name: 'georgiy', surname:'bochkarev'},
+{id: 5, name: 'matvei', surname:'gavrilov'}
+]
 
-	id+=1;
-	
-
+for (let i = 0; i < students.length; i++) {
+	let id = students[i].id
+	let name = students[i].name
+	let surname = students[i].surname
+	console.log('number: ' + id)
+	console.log('name: ' + name)
+	console.log('surname: ' + surname)
+	console.log('-------------------')
 }
