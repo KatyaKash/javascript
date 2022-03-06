084f665ade748c882a1d9c7649a3916b6a5ac5c7
KatyaKash <katya_kash@inbox.ru>
2022-03-06 22:01:54 +0700

    я че-то пыталась сделать но не могу проверить и доработать из-за этого

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let students = []
 let id_current = 0
 
-// загружаем файл с сайта
+// загружаем информацию с сайта
 function load_from_site(){
-//1. Создаем новый XMLHttpRequest-объект
+	// 1. Создаем новый XMLHttpRequest-объект
 	let xhr = new XMLHttpRequest();
-// 2. Настраиваем его: GET-запрос по URL http://217.71.129.139:4035/students.php
+	// 2. Настраиваем его: GET-запрос по URL http://217.71.129.139:4035/students.php
 	xhr.open('GET', 'http://217.71.129.139:4035/students.php');
-//3.Отсылаем запрос
+	// 3. Отсылаем запрос
 	xhr.send();
-//4.Сработает после того, как мы получим огтвет от сервера
+	// 4. Сработает после того, как мы получим огтвет от сервера
 	xhr.onload = function(){
 		if (xhr.status != 200){
 			alert(`Error ${xhr.status}: ${xhr.statusText}`); // например 404: Not Found
@@ -24,7 +24,6 @@ function load_from_site(){
 }
 
 
-
 function load_all() {
 	let table = document.getElementById('tbl_all')
 	for (let i = 0; i < students.length; i++) {
@@ -37,60 +36,63 @@ function load_all() {
 		let td1 = document.createElement('td')
 		let td2 = document.createElement('td')
 		let td3 = document.createElement('td')
+		let td4 = document.createElement('button#more')
+
 		//запишем данные в ячейк
 		td1.textContent = id;
 		td2.textContent = name;
 		td3.textContent = surname;
+		td4.textContent = 'Подробно';
+
 		//вставим ячейку в строку
 		tr.appendChild(td1);
 		tr.appendChild(td2);
 		tr.appendChild(td3);
+		tr.appendChild(td4);
+
 		//вставим строку в таблицу
 		table.appendChild(tr)
 	}
 }
-//let id_current = 0
-function load_student(id){
-	let img = document.getElementById('logo')
-	img.src = 'http://217.71.129.139:4035/'+ students[id].logo
-	let head = document.getElementById('zagolovok')
-	head.textContent = 'Info about student № '+ students[id].id
-	let span1 = document.getElementById('name')
-	span1.textContent = students[id_current].name
-	let span2 = document.getElementById('surname')
-	span2.textContent = students[id_current].surname
 
-	let scores = document.getElementById('scores')
+
+function load_student(id){
+	$('#logo').prop('src', 'http://217.71.129.139:4035/'+ students[id].logo) 
+	$('#zagolovok').prop('textContent','Информация о студенте № ' + students[id].id)
+	$('#name').prop('textContent', students[id].name)
+	$('#surname').prop('textContent', students[id].surname)
+	$('#scores').prop('textContent', students[id].scores)
+
 	let sum = 0;
 	for(let i = 0; i < students[id].scores.length; i++){
-		console.log(students[id].scores[i])
+		//console.log(students[id].scores[i])
 		sum+=students[id].scores[i]
 	}
-	scores.textContent = students[id].scores
-	//let sred = document.getElementById('sred')
-	sred.textContent = sum/(students[id].scores.length)
+	$('#sred').prop('textContent',  sum / (students[id].scores.length))
 }
 
 function Next(){
+	load_student(id_current)
 	id_current++
+
 	if (id_current > 0) {
-		document.getElementById('btn_prev').disabled = false
+		document.getElementById('btn_back').disabled = false
 	}
 	if (id_current===students.length-1){
 		document.getElementById('btn_next').disabled = true
-
 	}
-	load_student(id_current)
 }
-function Prev()	{
+function Back()	{
+	load_student(id_current)
 		id_current--
 	if (id_current === 0) {
-		document.getElementById('btn_prev').disabled = true
+		document.getElementById('btn_back').disabled = true
 	}
 	if (id_current > 0){
 		document.getElementById('btn_next').disabled = false
-
 	}
-	load_student(id_current)
 }
 
+$("#more").on("click"), function(){
+	load_student(0)
+}
